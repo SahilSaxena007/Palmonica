197995f4dc66045eb64c1e5d1a51f946d5f86d45
Sahil Saxena <sahil.saxena@student.manchester.ac.uk>
2025-10-24 17:48:19 +0100

    basic lower and upper octave set

--- harmonium/src/audio/keyMap.js
+++ harmonium/src/audio/keyMap.js
@@ -1,33 +1,33 @@
 export const keyMap = {
-  // Lower octave (C3–B3)
-  a: "C3", //w
-  s: "D3", //NOT WORKING
-  d: "E3", //NOT WORKING
-  f: "F3", // w
-  g: "G3", //NOT WORKING
-  h: "A3", // NOT WORKING
-  j: "B3", // NOT WORKING
+  // Lower octave white keys (C3–B3)
+  a: "C3",
+  s: "D3",
+  d: "E3",
+  f: "F3",
+  g: "G3",
+  h: "A3",
+  j: "B3",
 
   // Lower octave black keys
-  w: "C#3", // NOT WORKING
-  e: "D#3", // NOT WORKING
-  t: "F#3", // NOT WORKING
-  y: "G#3", // NOT WORKING
-  u: "A#3", // NOT WORKING
+  w: "C#3",
+  e: "D#3",
+  t: "F#3",
+  y: "G#3",
+  u: "A#3",
 
-  // Upper octave (C4–B4)
-  z: "C4", // w
-  x: "D4", // w
-  c: "E4", // w
-  v: "F4", // w
-  b: "G4", // w
-  n: "A4", // w
-  m: "B4", // w
+  // Upper octave white keys (C4–B4)
+  z: "C4",
+  x: "D4",
+  c: "E4",
+  v: "F4",
+  b: "G4",
+  n: "A4",
+  m: "B4",
 
   // Upper octave black keys
-  s: "C#4", // NOT WORKING (repeated)
-  d: "D#4", // NOT WORKING(repeated)
-  g: "F#4", // NOT WORKING (repeated)
-  h: "G#4", // NOT WORKING (repeated)
-  j: "A#4", // NOT WORKING (repeated)
+  r: "C#4",
+  // f: "D#4",
+  // j: "F#4",
+  k: "G#4",
+  l: "A#4",
 };
--- harmonium/src/audio/soundEngine.js
+++ harmonium/src/audio/soundEngine.js
@@ -3,67 +3,51 @@ import * as Tone from "tone";
 const harmonium = new Tone.Sampler({
   urls: {
     // A
-    "A#2": "sounds/harmonium-a#2.wav",
-    "A#3": "sounds/harmonium-a#3.wav", // NOT WORKING
-    "A#4": "sounds/harmonium-a#4.wav", // NOT WORKING
-    A2: "sounds/harmonium-a2.wav", // no mapping at all
-    A3: "sounds/harmonium-a3.wav", // NOT WORKING
+    "A#3": "sounds/harmonium-a#3.wav",
+    "A#4": "sounds/harmonium-a#4.wav",
+    A3: "sounds/harmonium-a3.wav",
     A4: "sounds/harmonium-a4.wav",
 
     // B
-    B2: "sounds/harmonium-b2.wav", // no mapping at all
-    B3: "sounds/harmonium-b3.wav", // NOT WORKING
+    B3: "sounds/harmonium-b3.wav",
     B4: "sounds/harmonium-b4.wav",
 
     // C
-    "C#2": "sounds/harmonium-c#2.wav",
-    "C#3": "sounds/harmonium-c#3.wav", // NOT WORKING
-    "C#4": "sounds/harmonium-c#4.wav", // NOT WORKING
-    "C#5": "sounds/harmonium-c#5.wav", // no mapping at all
-    C2: "sounds/harmonium-c2.wav", // no mapping at all
-    C3: "sounds/harmonium-c3.wav", // w
+    "C#3": "sounds/harmonium-c#3.wav",
+    "C#4": "sounds/harmonium-c#4.wav",
+    C3: "sounds/harmonium-c3.wav",
     C4: "sounds/harmonium-c4.wav",
-    C5: "sounds/harmonium-c5.wav", // no mapping at all
 
     // D
-    "D#2": "sounds/harmonium-d#2.wav",
-    "D#3": "sounds/harmonium-d#3.wav", // NOT WORKING
-    "D#4": "sounds/harmonium-d#4.wav", // NOT WORKING
-    D2: "sounds/harmonium-d2.wav", // no mapping at all
-    D3: "sounds/harmonium-d3.wav", //not working
+    "D#3": "sounds/harmonium-d#3.wav",
+    "D#4": "sounds/harmonium-d#4.wav",
+    D3: "sounds/harmonium-d3.wav",
     D4: "sounds/harmonium-d4.wav",
-    D5: "sounds/harmonium-d5.wav", // no mapping at all
 
     // E
-    E2: "sounds/harmonium-e2.wav", // no mapping at all
-    E3: "sounds/harmonium-e3.wav", //NOT
+    E3: "sounds/harmonium-e3.wav",
     E4: "sounds/harmonium-e4.wav",
 
     // F
-    "F#2": "sounds/harmonium-f#2.wav",
-    "F#3": "sounds/harmonium-f#3.wav", // NOT WORKING
-    "F#4": "sounds/harmonium-f#4.wav", // NOT WORKING
-    F2: "sounds/harmonium-f2.wav", // no mapping at all
-    F3: "sounds/harmonium-f3.wav", //w
+    "F#3": "sounds/harmonium-f#3.wav",
+    "F#4": "sounds/harmonium-f#4.wav",
+    F3: "sounds/harmonium-f3.wav",
     F4: "sounds/harmonium-f4.wav",
 
     // G
-    "G#2": "sounds/harmonium-g#2.wav",
-    "G#3": "sounds/harmonium-g#3.wav", // NOT WORKING
-    "G#4": "sounds/harmonium-g#4.wav", // NOT WORKING
-    G2: "sounds/harmonium-g2.wav", // no mapping at all
-    G3: "sounds/harmonium-g3.wav", // NOT WORKING
+    "G#3": "sounds/harmonium-g#3.wav",
+    "G#4": "sounds/harmonium-g#4.wav",
+    G3: "sounds/harmonium-g3.wav",
     G4: "sounds/harmonium-g4.wav",
   },
   release: 1.2,
   baseUrl: "/",
 }).toDestination();
 
-// Add light reverb for realism
+// Add light reverb for warmth
 const reverb = new Tone.Reverb(2).toDestination();
 harmonium.connect(reverb);
 
-// init + sample loading
 export async function initAudio() {
   if (Tone.context.state !== "running") await Tone.start();
   await Tone.loaded();
